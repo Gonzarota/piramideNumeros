f215f81ada18f52c58a8b595fe4b04d491c3824a
Gonzarota <gonzape93@gmail.com>
2020-07-07 10:49:33 -0400

    Semipiramide, invertida

--- src/PiramideNumeros.java
+++ src/PiramideNumeros.java
@@ -6,12 +6,22 @@ public class PiramideNumeros {
         Scanner input= new Scanner(System.in);
         int fila=input.nextInt();
 
-        for(int i=1;i<=fila;i++){
+        /*for(int i=1;i<=fila;i++){
             for(int j=1;j<=i;j++){
                 System.out.print(j);
             }
             System.out.println();
         }
+
+        */
+
+        for(int i=fila;i>=0;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
 
     }
 }
