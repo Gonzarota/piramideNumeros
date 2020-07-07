2ea74fe6aa79fcdff6c31b85fe8200f8f003814e
Gonzarota <gonzape93@gmail.com>
2020-07-07 10:17:40 -0400

    Semipiramide

---
+++ src/PiramideNumeros.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class PiramideNumeros {
+    public static void main(String[] args) {
+        System.out.println("Dime el número de filas que quieres");
+        Scanner input= new Scanner(System.in);
+        int fila=input.nextInt();
+
+        for(int i=1;i<=fila;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+    }
+}
